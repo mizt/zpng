b9fc9244ae6015331b632c1cf16d6b75200e8109
mizt <mizt@M1.local>
2021-08-05 11:36:12 +0900

    Update

--- emscripten/decode/decode.cpp
+++ emscripten/decode/decode.cpp
@@ -5,7 +5,7 @@ Filter *filter = nullptr;
 
 extern "C" {
     void decode(unsigned char *dst,int w, int h,unsigned char *src,unsigned int length) {
-        if(filter==nullptr) filter = new Filter(w,h);
+        if(filter==nullptr) filter = new SubtractGreenFilter(w,h);
         ZSTD_decompress(dst,(w*h)<<2,src,length);
         filter->add((unsigned int *)dst);
     }
