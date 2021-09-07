5218a6610f60eed0e2d155c305f87bac4a9f3326
mizt <info@mizt.org>
2021-09-08 01:25:42 +0900

    Update

--- encode/main.mm
+++ encode/main.mm
@@ -10,9 +10,8 @@ int main(int argc, const char * argv[]) {
     int w = 1920;
     int h = 1080;
     
-    Filter *filter = new Filter(w,h);
+    Filter *filter = new Filter(w,h,Filter::RGB);
   
-    unsigned int *dst = new unsigned int[w*h];
     unsigned int *src = new unsigned int[w*h];
     for(int i=0; i<h; i++) {
         for(int j=0; j<w; j++) {
@@ -20,11 +19,17 @@ int main(int argc, const char * argv[]) {
         }
     }
     
-    QTZPNGRecorder *recorder = new QTZPNGRecorder(w,h,30,@"./test.mov");
-    filter->sub(src);
-    size_t len = ZSTD_compress(dst,(w*h)<<2,src,(w*h)<<2,1);
-    recorder->add((unsigned char *)dst,(unsigned int)len);
+    QTZPNGRecorder *recorder = new QTZPNGRecorder(w,h,30,8*filter->bpp(),@"./test.mov");
+    filter->encode((unsigned char *)src,4,5);
+    unsigned char *dst = new unsigned char[filter->length()];
+    size_t len = ZSTD_compress(dst,w*h*filter->bpp(),filter->bytes(),filter->length(),1);
+    recorder->add(dst,(unsigned int)len);
+    recorder->add(dst,(unsigned int)len);
+    recorder->add(dst,(unsigned int)len);
     recorder->save();
+    
+    delete[] dst;
+    delete[] src;
 
     return 0;
 }
