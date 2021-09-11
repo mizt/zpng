8528ec26a8e640a02b8ca52abec9702f17172623
mizt <info@mizt.org>
2021-09-12 06:30:33 +0900

    Update emscripten/decode

--- emscripten/decode/decode.cpp
+++ emscripten/decode/decode.cpp
@@ -1,12 +1,16 @@
 #include <zstd.h>
+#include <math.h>
 #include "Filter.h"
 
-Filter *filter = nullptr;
+Filter::Decoder *decoder = nullptr;
 
 extern "C" {
-    void decode(unsigned char *dst,int w, int h,unsigned char *src,unsigned int length) {
-        if(filter==nullptr) filter = new SubtractGreenFilter(w,h);
-        ZSTD_decompress(dst,(w*h)<<2,src,length);
-        filter->add((unsigned int *)dst);
+    void decode(unsigned char *dst,int w, int h, int bpp, unsigned char *src, unsigned int length) {
+        if(decoder==nullptr) decoder = new Filter::Decoder(w,h,Filter::RGBA);
+        ZSTD_decompress(dst,(w*h)*4,src,length);
+        decoder->decode(dst,bpp);
+        unsigned int *p = (unsigned int *)decoder->bytes();
+        unsigned int *q = (unsigned int *)dst;
+        for(int k=0; k<w*h; k++) q[k] = p[k];
     }
 }
