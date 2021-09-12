f81d56ebb7c9cf427f30072ae8fc6d0f2095d00d
mizt <info@mizt.org>
2021-09-12 10:22:05 +0900

    Update Premiere Pro importers

--- premiere/SDK_File_Import.mm
+++ premiere/SDK_File_Import.mm
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 #import "SDK_File_Import.h"
 #import <assert.h>
-#import <utility>
+#import <iostream>
 #import <zstd.h>
 #import "Filter.h"
 
@@ -28,24 +28,13 @@ typedef struct {
     PrSDKTimeSuite *TimeSuite;
     PrSDKImporterFileManagerSuite *FileSuite;
     unsigned char *zpng;
-    unsigned int *buffer;
-    std::pair<unsigned int,unsigned int> *frames;
-    SubtractGreenFilter *filter;
+    unsigned char *buffer;
+    unsigned int depth;
+    unsigned int *frame_head;
+    unsigned int *frame_size;
+    Filter::Decoder *decoder;
 } ImporterLocalRec8, *ImporterLocalRec8Ptr, **ImporterLocalRec8H;
 
-static unsigned int atom(std::string str) {
-    assert(str.length()==4);
-    unsigned char *key =(unsigned char *)str.c_str();
-    return key[0]<<24|key[1]<<16|key[2]<<8|key[3];
-}
-
-static unsigned short swapU16(unsigned short n) {
-    return ((n>>8)&0xFF)|((n&0xFF)<<8);
-}
-
-static unsigned int swapU32(unsigned int n) {
-    return ((n>>24)&0xFF)|(((n>>16)&0xFF)<<8)|(((n>>8)&0xFF)<<16)|((n&0xFF)<<24);
-}
 
 static prMALError SDKInit(imStdParms *stdParms, imImportInfoRec *importInfo) {
     importInfo->canSave = kPrFalse;
@@ -64,12 +53,12 @@ static prMALError SDKInit(imStdParms *stdParms, imImportInfoRec *importInfo) {
 static prMALError SDKGetIndFormat(imStdParms *stdParms, csSDK_size_t index, imIndFormatRec *SDKIndFormatRec) {
         
     prMALError result = malNoError;
-    
     char formatname[255] = "zpng";
     char shortname[32] = "zpng";
     char platformXten[256] = "zpng\0";
 
     switch(index) {
+        
         case 0:
             SDKIndFormatRec->filetype = 'zpng';
             SDKIndFormatRec->canWriteTimecode = kPrFalse;
@@ -87,9 +76,11 @@ static prMALError SDKGetIndFormat(imStdParms *stdParms, csSDK_size_t index, imIn
     return result;
 }
 
+
 prMALError SDKOpenFile8(imStdParms *stdParms, imFileRef *SDKfileRef, imFileOpenRec8 *SDKfileOpenRec8) {
         
     prMALError result = malNoError;
+
     ImporterLocalRec8H localRecH = NULL;
     ImporterLocalRec8Ptr localRecP = NULL;
 
@@ -99,25 +90,33 @@ prMALError SDKOpenFile8(imStdParms *stdParms, imFileRef *SDKfileRef, imFileOpenR
         localRecP = reinterpret_cast<ImporterLocalRec8Ptr>(*localRecH);
     }
     else {
+        
         localRecH = (ImporterLocalRec8H)stdParms->piSuites->memFuncs->newHandle(sizeof(ImporterLocalRec8));
         SDKfileOpenRec8->privatedata = (PrivateDataPtr)localRecH;
         stdParms->piSuites->memFuncs->lockHandle(reinterpret_cast<char**>(localRecH));
         localRecP = reinterpret_cast<ImporterLocalRec8Ptr>(*localRecH);
+        
         localRecP->memFuncs = stdParms->piSuites->memFuncs;
         localRecP->BasicSuite = stdParms->piSuites->utilFuncs->getSPBasicSuite();
         if(localRecP->BasicSuite) {
             localRecP->BasicSuite->AcquireSuite(kPrSDKPPixCreatorSuite,kPrSDKPPixCreatorSuiteVersion,(const void**)&localRecP->PPixCreatorSuite);
-            localRecP->BasicSuite->AcquireSuite(kPrSDKPPixCacheSuite, PrCacheVersion,(const void**)&localRecP->PPixCacheSuite);
+            localRecP->BasicSuite->AcquireSuite(kPrSDKPPixCacheSuite,PrCacheVersion,(const void**)&localRecP->PPixCacheSuite);
             localRecP->BasicSuite->AcquireSuite(kPrSDKPPixSuite,kPrSDKPPixSuiteVersion,(const void**)&localRecP->PPixSuite);
             localRecP->BasicSuite->AcquireSuite(kPrSDKTimeSuite,kPrSDKTimeSuiteVersion,(const void**)&localRecP->TimeSuite);
             localRecP->BasicSuite->AcquireSuite(kPrSDKImporterFileManagerSuite,kPrSDKImporterFileManagerSuiteVersion,(const void**)&localRecP->FileSuite);
         }
+
         localRecP->importerID = SDKfileOpenRec8->inImporterID;
         localRecP->fileType = SDKfileOpenRec8->fileinfo.filetype;
-        localRecP->frames = nullptr;
+        
+        localRecP->depth = 0;
+        
+        localRecP->frame_head = nullptr;
+        localRecP->frame_size = nullptr;
+        
         localRecP->zpng = nullptr;
         localRecP->buffer = nullptr;
-        localRecP->filter = nullptr;
+        localRecP->decoder = nullptr;
     }
 
     SDKfileOpenRec8->fileinfo.fileref = *SDKfileRef = reinterpret_cast<imFileRef>(imInvalidHandleValue);
@@ -129,7 +128,7 @@ prMALError SDKOpenFile8(imStdParms *stdParms, imFileRef *SDKfileRef, imFileOpenR
         CFURLRef filePathURL = CFURLCreateWithFileSystemPath(NULL,filePathCFSR,kCFURLPOSIXPathStyle,false);
         if(filePathURL != NULL) {
             FSRef fileRef;
-            Boolean success = CFURLGetFSRef(filePathURL, &fileRef);
+            Boolean success = CFURLGetFSRef(filePathURL,&fileRef);
             if(success) {
                 HFSUniStr255 dataForkName;
                 FSGetDataForkName(&dataForkName);
@@ -165,8 +164,8 @@ prMALError SDKOpenFile8(imStdParms *stdParms, imFileRef *SDKfileRef, imFileOpenR
 }
 
 static prMALError SDKQuietFile(imStdParms *stdParms, imFileRef *SDKfileRef,void *privateData) {
-        
-    if(SDKfileRef && *SDKfileRef!=imInvalidHandleValue) {
+
+    if(SDKfileRef && *SDKfileRef != imInvalidHandleValue) {
         ImporterLocalRec8H ldataH = reinterpret_cast<ImporterLocalRec8H>(privateData);
         stdParms->piSuites->memFuncs->lockHandle(reinterpret_cast<char**>(ldataH));
         stdParms->piSuites->memFuncs->unlockHandle(reinterpret_cast<char**>(ldataH));
@@ -181,11 +180,9 @@ static prMALError SDKCloseFile(imStdParms *stdParms, imFileRef *SDKfileRef, void
         
     ImporterLocalRec8H ldataH = reinterpret_cast<ImporterLocalRec8H>(privateData);
     
-    if(SDKfileRef && *SDKfileRef!=imInvalidHandleValue) {
-        SDKQuietFile(stdParms,SDKfileRef,privateData);
-    }
+    if(SDKfileRef && *SDKfileRef!=imInvalidHandleValue) SDKQuietFile(stdParms,SDKfileRef,privateData);
 
-    if(ldataH && *ldataH && (*ldataH)->BasicSuite) {
+    if(ldataH&&*ldataH&&(*ldataH)->BasicSuite) {
         stdParms->piSuites->memFuncs->lockHandle(reinterpret_cast<char**>(ldataH));
         ImporterLocalRec8Ptr localRecP = reinterpret_cast<ImporterLocalRec8Ptr>(*ldataH);
         localRecP->BasicSuite->ReleaseSuite(kPrSDKPPixCreatorSuite,kPrSDKPPixCreatorSuiteVersion);
@@ -205,11 +202,26 @@ static prMALError SDKGetIndPixelFormat(imStdParms *stdParms, csSDK_size_t idx,im
         case 0:
             SDKIndPixelFormatRec->outPixelFormat = PrPixelFormat_BGRA_4444_8u;
             break;
+    
         default:
             result = imBadFormatIndex;
             break;
     }
     return result;
+}
+
+static unsigned int atom(std::string str) {
+    assert(str.length()==4);
+    unsigned char *key =(unsigned char *)str.c_str();
+    return key[0]<<24|key[1]<<16|key[2]<<8|key[3];
+}
+
+static unsigned short swapU16(unsigned short n) {
+    return ((n>>8)&0xFF)|((n&0xFF)<<8);
+}
+
+static unsigned int swapU32(unsigned int n) {
+    return ((n>>24)&0xFF)|(((n>>16)&0xFF)<<8)|(((n>>8)&0xFF)<<16)|((n&0xFF)<<24);
 }
 
 prMALError SDKGetInfo8(imStdParms *stdParms, imFileAccessRec8 *fileAccessInfo8, imFileInfoRec8 *SDKFileInfo8) {
@@ -230,45 +242,38 @@ prMALError SDKGetInfo8(imStdParms *stdParms, imFileAccessRec8 *fileAccessInfo8, 
     SDKFileInfo8->hasAudio = kPrFalse;
     
     if(localRecP) {
-        
         int frames = 0;
         unsigned int type = atom("zpng");
         unsigned int buffer = 0;
-        
         ByteCount actualCount = 0;
         FSReadFork((FSIORefNum)CAST_REFNUM(fileAccessInfo8->fileref),fsFromStart,4*7,4,&buffer,&actualCount);
-        
         int offset = swapU32(buffer);
         FSReadFork((FSIORefNum)CAST_REFNUM(fileAccessInfo8->fileref),fsFromStart,4*8,4,&buffer,&actualCount);
-        
         if(swapU32(buffer)==atom("mdat")) {
-            
             FSReadFork((FSIORefNum)CAST_REFNUM(fileAccessInfo8->fileref),fsFromStart,(4*8)+offset-4,4,&buffer,&actualCount);
             int len = swapU32(buffer);
-            
             FSReadFork((FSIORefNum)CAST_REFNUM(fileAccessInfo8->fileref),fsFromStart,(4*8)+offset,4,&buffer,&actualCount);
-            
             if(swapU32(buffer)==atom("moov")) {
                 
                 unsigned char *moov = new unsigned char[len-8];
-                FSReadFork((FSIORefNum)CAST_REFNUM(fileAccessInfo8->fileref),fsFromStart,(4*8)+offset+4,len-8,moov,&actualCount);
                 
+                FSReadFork((FSIORefNum)CAST_REFNUM(fileAccessInfo8->fileref),fsFromStart,(4*8)+offset+4,len-8,moov,&actualCount);                
                 bool key = false;
                 int seek = ((4*4)+4+6+2+2+2+4+4+4);
-                
-                for(int k=0; k<(len-8)-(seek+2*2); k++) {
-                    
+                for(int k=0; k<(len-8)-(seek+(2)*2+(4+4+4+2+32+2)+2); k++) {
                     if(swapU32(*((unsigned int *)(moov+k)))==atom("stsd")) {
                         if(swapU32(*((unsigned int *)(moov+k+(4*4))))==type) {
-                            SDKFileInfo8->vidInfo.imageWidth = swapU16(*((unsigned short *)(moov+k+seek)));
+                            SDKFileInfo8->vidInfo.imageWidth  = swapU16(*((unsigned short *)(moov+k+seek)));
                             SDKFileInfo8->vidInfo.imageHeight = swapU16(*((unsigned short *)(moov+k+seek+2)));
+                            localRecP->depth = (swapU16(*((unsigned short *)(moov+k+seek+2+4+4+4+2+32+2))))>>3;
                             key = true;
                             break;
                         }
                     }
                 }
-                            
+            
                 if(key) {
+                    
                     unsigned int TimeScale = 0;
                     for(int k=0; k<(len-8)-3; k++) {
                         if(swapU32(*((unsigned int *)(moov+k)))==atom("mvhd")) {
@@ -297,16 +302,19 @@ prMALError SDKGetInfo8(imStdParms *stdParms, imFileAccessRec8 *fileAccessInfo8, 
                                 if(swapU32(*((unsigned int *)(moov+k)))==atom("stsz")) {
                                     k+=(4*3);
                                     if(k<(len-8)) {
-                                        unsigned int chunk = 4*9;
+                                        unsigned int head = 4*9;
                                         frames = swapU32(*((unsigned int *)(moov+k)));
-                                        if(localRecP->frames) delete[] localRecP->frames;
-                                        localRecP->frames = new std::pair<unsigned int,unsigned int>[frames];
+                                        if(localRecP->frame_head) delete[] localRecP->frame_head;
+                                        if(localRecP->frame_size) delete[] localRecP->frame_size;
+                                        localRecP->frame_head = new unsigned int[frames];
+                                        localRecP->frame_size = new unsigned int[frames];
                                         for(int f=0; f<frames; f++) {
                                             k+=4;
                                             if(k<len-8) {
                                                 unsigned int size = swapU32(*((unsigned int *)(moov+k)));
-                                                localRecP->frames[f] = std::make_pair(chunk,size);
-                                                chunk+=size;
+                                                localRecP->frame_head[f] = head;
+                                                localRecP->frame_size[f] = size;
+                                                head+=size;
                                             }
                                         }
                                         break;
@@ -332,7 +340,7 @@ prMALError SDKGetInfo8(imStdParms *stdParms, imFileAccessRec8 *fileAccessInfo8, 
             }
         }
                 
-        if(frames>0) {
+        if((localRecP->depth==3||localRecP->depth==4)&&frames>0) {
             
             SDKFileInfo8->hasVideo = kPrTrue;
             SDKFileInfo8->vidInfo.subType = PrPixelFormat_BGRA_4444_8u;
@@ -340,9 +348,11 @@ prMALError SDKGetInfo8(imStdParms *stdParms, imFileAccessRec8 *fileAccessInfo8, 
             SDKFileInfo8->vidInfo.fieldType = prFieldsUnknown;
             SDKFileInfo8->vidInfo.isStill = kPrFalse;
             SDKFileInfo8->vidInfo.noDuration = imNoDurationFalse;
+            
             SDKFileInfo8->vidInfo.alphaType = alphaStraight;
-            SDKFileInfo8->vidDuration = frames*SDKFileInfo8->vidSampleSize;
-          
+            
+            SDKFileInfo8->vidDuration = frames * SDKFileInfo8->vidSampleSize;
+            
             localRecP->width = SDKFileInfo8->vidInfo.imageWidth;
             localRecP->height = SDKFileInfo8->vidInfo.imageHeight;
             
@@ -359,14 +369,14 @@ prMALError SDKGetInfo8(imStdParms *stdParms, imFileAccessRec8 *fileAccessInfo8, 
                 localRecP->buffer  = nullptr;
             }
             
-            if(localRecP->filter) {
-                delete[]  localRecP->filter;
-                localRecP->filter = nullptr;
+            if(localRecP->decoder) {
+                delete localRecP->decoder;
+                localRecP->decoder = nullptr;
             }
             
             localRecP->zpng = new unsigned char[localRecP->width*localRecP->height*4];
-            localRecP->buffer = new unsigned int[localRecP->width*localRecP->height];
-            localRecP->filter = new SubtractGreenFilter(localRecP->width,localRecP->height);
+            localRecP->buffer = new unsigned char[localRecP->width*localRecP->height*4+localRecP->height];
+            localRecP->decoder = new Filter::Decoder(localRecP->width,localRecP->height,Filter::RGBA);
                         
         }
         else {
@@ -405,36 +415,43 @@ static prMALError SDKPreferredFrameSize(imStdParms *stdparms, imPreferredFrameSi
     return result;
 }
 
+
 static prMALError SDKGetSourceVideo(imStdParms *stdParms, imFileRef fileRef, imSourceVideoRec *sourceVideoRec) {
     
     ImporterLocalRec8H ldataH = reinterpret_cast<ImporterLocalRec8H>(sourceVideoRec->inPrivateData);
     stdParms->piSuites->memFuncs->lockHandle(reinterpret_cast<char**>(ldataH));
-    ImporterLocalRec8Ptr localRecP = reinterpret_cast<ImporterLocalRec8Ptr>(*ldataH);
+    ImporterLocalRec8Ptr localRecP = reinterpret_cast<ImporterLocalRec8Ptr>( *ldataH );
 
     PrTime ticksPerSecond = 0;
     localRecP->TimeSuite->GetTicksPerSecond(&ticksPerSecond);
     
     csSDK_int32 theFrame = 0;
-    if(localRecP->frameRateDen!=0) {
-        PrTime ticksPerFrame = (ticksPerSecond*(PrTime)localRecP->frameRateDen)/(PrTime)localRecP->frameRateNum;
+    if(localRecP->frameRateDen==0) {
+        theFrame = 0;
+    }
+    else {
+        PrTime ticksPerFrame = (ticksPerSecond * (PrTime)localRecP->frameRateDen) / (PrTime)localRecP->frameRateNum;
         theFrame = (csSDK_int32)(sourceVideoRec->inFrameTime/ticksPerFrame);
     }
 
     prMALError result = localRecP->PPixCacheSuite->GetFrameFromCache(localRecP->importerID,0,theFrame,1,sourceVideoRec->inFrameFormats,sourceVideoRec->outFrame,NULL,NULL);
-    if(result!=suiteError_NoError) {
+
+    if(result != suiteError_NoError) {
         result = malNoError;
+        
         imFrameFormat *frameFormat = &sourceVideoRec->inFrameFormats[0];
         prRect theRect;
         if(frameFormat->inFrameWidth==0&&frameFormat->inFrameHeight==0) {
             frameFormat->inFrameWidth = localRecP->width;
             frameFormat->inFrameHeight = localRecP->height;
         }
+        
         prSetRect(&theRect,0,0,frameFormat->inFrameWidth,frameFormat->inFrameHeight);
         
         PPixHand ppix;
         localRecP->PPixCreatorSuite->CreatePPix(&ppix,PrPPixBufferAccess_ReadWrite,frameFormat->inPixelFormat,&theRect);
         
-        if(frameFormat->inPixelFormat==PrPixelFormat_BGRA_4444_8u) {
+        if(frameFormat->inPixelFormat == PrPixelFormat_BGRA_4444_8u) {
             
             char *pixelAddress = nullptr;
             localRecP->PPixSuite->GetPixels(ppix,PrPPixBufferAccess_ReadWrite,&pixelAddress);
@@ -446,20 +463,28 @@ static prMALError SDKGetSourceVideo(imStdParms *stdParms, imFileRef fileRef, imS
             
             assert(frameFormat->inFrameWidth==w);
             assert(frameFormat->inFrameHeight==h);
-            
+                        
+            int frame_head = localRecP->frame_head[theFrame];
+            int frame_size = localRecP->frame_size[theFrame];
+                        
             ByteCount actualCount = 0;
-            unsigned int chunk = localRecP->frames[theFrame].first;
-            unsigned int size = localRecP->frames[theFrame].second;
-            FSReadFork((FSIORefNum)CAST_REFNUM(fileRef),fsFromStart,chunk,size,localRecP->zpng,&actualCount);
-            ZSTD_decompress(localRecP->buffer,(w*h)<<2,localRecP->zpng,size);
-            localRecP->filter->add(localRecP->buffer);
+            FSReadFork((FSIORefNum)CAST_REFNUM(fileRef),fsFromStart,frame_head,frame_size,localRecP->zpng,&actualCount);
+            ZSTD_decompress(localRecP->buffer,(w*h)*4+h,localRecP->zpng,frame_size);
+            localRecP->decoder->decode((unsigned char *)localRecP->buffer,localRecP->depth);
+
+            unsigned int *bytes = (unsigned int *)localRecP->decoder->bytes();
+
             for(int i=0; i<h; i++) {
-                unsigned int *p = (unsigned int *)(pixelAddress+(i*rowBytes));
+                unsigned char *p = (unsigned char *)pixelAddress+(i*rowBytes);
                 for(int j=0; j<w; j++) {
-                    unsigned int pixel = localRecP->buffer[i*w+j];
-                    *p++ = (pixel&0xFF00FF00)|(pixel&0xFF)<<16|((pixel>>16)&0xFF);
+                    unsigned int pixel = bytes[((h-1)-i)*w+j];
+                    *p++ = (pixel>>16)&0xFF;
+                    *p++ = (pixel>>8)&0xFF;
+                    *p++ = (pixel)&0xFF;
+                    *p++ = (pixel>>24)&0xFF;;
                 }
             }
+            
             localRecP->PPixCacheSuite->AddFrameToCache(localRecP->importerID,0,ppix,theFrame,NULL,NULL);
             *sourceVideoRec->outFrame = ppix;
         }
@@ -467,7 +492,9 @@ static prMALError SDKGetSourceVideo(imStdParms *stdParms, imFileRef fileRef, imS
             assert(false);
         }
     }
+
     stdParms->piSuites->memFuncs->unlockHandle(reinterpret_cast<char**>(ldataH));
+
     return result;
 }
 
